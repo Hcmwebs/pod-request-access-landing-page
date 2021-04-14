57bfd7e7a6dde01859b9b99878efe0f33440c0cf
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-04-15 09:15:07 +1200

    fixed the form validation and reseting the form if validation is corrrect

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
+const formGroup = document.getElementById('formGroup');
+const errMsg = document.getElementById('errorMsg');
 
 
 
@@ -7,40 +9,33 @@ const isEmail = (email) =>{
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
-const setError = (input, message) => {
-	const formGroup = input.parentElement;
-	const small = formGroup.querySelector('small');
+const setError = (message) => {
 	formGroup.className = 'formGroup error';
-	small.innerText = message;
+	errMsg.innerText = message;
 }
 
-function setSuccess(input) {
-	const formGroup =input.parentElement;
-	formGroup.className = 'formGroup success';
+const setSuccess = () => {
+	email.value = '';
 }
 
 const formValidation = () => {
   const emailValue = email.value.trim();
 
   if(emailValue === '') {
-		setError(email, 'Oops! Please add your email');
+		setError('Oops! Please add your email');
 	} else if (!isEmail(emailValue)) {
-		setError(email, 'Oops! Please check your email');
+		setError('Oops! Please check your email');
 	} else {
-		setSuccess(email)
-  
+		setSuccess();
 	}
-
 
 }
 
 
-const handleSubmit = e => {
+const handleSubmit = (e) => {
   e.preventDefault();
-  const validated = formValidation();
-  if(validated){
-    email.innerHTML = '';
-  }
+  formValidation();
+
 }
 
 
